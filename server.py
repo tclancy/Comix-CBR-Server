389f83583f34d1e0a9e4834b409079477fe47d33
Tom Clancy <tclancy@gmail.com>
2011-11-28 18:53:44 -0500

    Got directory info passed in

--- server.py
+++ server.py
@@ -3,22 +3,26 @@ import ConfigParser
 from twisted.web import server, resource
 from twisted.internet import reactor
 
-class HelloResource(resource.Resource):
+class CBRResource(resource.Resource):
     isLeaf = True
-    numberRequests = 0
+    
+    def __init__(self, directory):
+        # old-skool call to parent
+        resource.Resource.__init__(self)
+        self.directory = directory
 
     def render_GET(self, request):
-        self.numberRequests += 1
         request.setHeader("content-type", "text/plain")
-        return "I am request #" + str(self.numberRequests)
+        return "Serving contents of %s" % self.directory
 
 
 config = ConfigParser.ConfigParser()
 try:
     config.read("comix.conf")
     port = int(config.get("basics", "port"))
-    directory = config.get("basics", "directory")
-    reactor.listenTCP(port, server.Site(HelloResource()))
+    reactor.listenTCP(port, server.Site(
+        CBRResource(config.get("basics", "directory")))
+    )
     reactor.run()
 except ConfigParser.ParsingError, e:
     print """Sorry, I couldn't find a comix.conf file in this directory.

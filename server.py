004515fb5af34a4e1cd735153f8a6ee2f946a970
Tom Clancy <tclancy@gmail.com>
2011-11-28 18:43:21 -0500

    Added simple configuration file setup.

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import ConfigParser
+
 from twisted.web import server, resource
 from twisted.internet import reactor
 
@@ -10,5 +12,17 @@ class HelloResource(resource.Resource):
         request.setHeader("content-type", "text/plain")
         return "I am request #" + str(self.numberRequests)
 
-reactor.listenTCP(8080, server.Site(HelloResource()))
-reactor.run()
+
+config = ConfigParser.ConfigParser()
+try:
+    config.read("comix.conf")
+    port = int(config.get("basics", "port"))
+    directory = config.get("basics", "directory")
+    reactor.listenTCP(port, server.Site(HelloResource()))
+    reactor.run()
+except ConfigParser.ParsingError, e:
+    print """Sorry, I couldn't find a comix.conf file in this directory.
+It should contain a [basics] section with port and directory info"""
+    sys.exit(1)
+except ValueError, e:
+    print "The value for port in comix.conf must be a number"

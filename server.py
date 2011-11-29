ff8f6e593310696c35c97d8e8615c6841521b655
Tom Clancy <tclancy@gmail.com>
2011-11-29 13:59:53 -0500

    Added logging and initial pass at finding files.

--- server.py
+++ server.py
@@ -1,8 +1,32 @@
 import ConfigParser
-import glob
+import fnmatch
+import logging
+import os
 
 from twisted.web import server, resource
 from twisted.internet import reactor
+
+# Setup logging
+logger = logging.getLogger("comix")
+logger.setLevel(logging.DEBUG)
+
+# Use file output for production logging:
+logfilename = "comix.log"
+filelog = logging.FileHandler(logfilename, "w")
+filelog.setLevel(logging.INFO)
+
+# Use console for development logging:
+conlog = logging.StreamHandler()
+conlog.setLevel(logging.DEBUG)
+
+# Specify log formatting:
+formatter = logging.Formatter("%(asctime)s - %(message)s")
+conlog.setFormatter(formatter)
+filelog.setFormatter(formatter)
+
+# Add console log to logger - TODO: check config file before turning on console logging
+logger.addHandler(conlog)
+logger.addHandler(filelog)
 
 class CBRResource(resource.Resource):
     isLeaf = True
@@ -14,18 +38,31 @@ class CBRResource(resource.Resource):
         
         # TODO: directory handling - make sure ends in /,
         # replace Windows separator stuff with /
-        self.directory = directory
+        self.directory = self._normalize_directory_path(directory)
+        if not os.path.exists(self.directory):
+            logger.critical("%s is not a valid root directory" % self.directory)
+            sys.exit(1)
         
-        # TODO: get CBR files via http://stackoverflow.com/questions/2186525/use-a-glob-to-find-files-recursively-in-python
         # when you find a cbr or cbz, put folder name into titles
-        self.dirlist = glob.glob('%s*' % self.directory)
+        self.files = []
+        for root, dirnames, filenames in os.walk(self.directory):
+            matches = fnmatch.filter(filenames, "*.cb[r|z]")
+            for f in matches:
+                self.files.append(os.path.join(root, f))
+        logger.info("Found %d files" % len(self.files))
 
     def render_GET(self, request):
         request.setHeader("content-type", "text/html")
         response = "Serving contents of %s" % self.directory
-        for d in self.dirlist:
+        for d in self.files:
             response += "<br />%s" % d
         return response
+    
+    def _normalize_directory_path(self, directory):
+        """
+        For Windows, get rid of \ crud
+        """
+        return directory.replace('\\', '/')
 
 
 config = ConfigParser.ConfigParser()

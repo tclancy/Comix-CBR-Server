b6ccb527da5a2380c97e7536beff9e0c89b197f2
Tom Clancy <tclancy@gmail.com>
2011-11-29 15:06:29 -0500

    Starting to clean up title selections.

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import ConfigParser
 import fnmatch
 import logging
 import os
+import re
 
 from twisted.web import server, resource
 from twisted.internet import reactor
@@ -28,6 +29,16 @@ filelog.setFormatter(formatter)
 logger.addHandler(conlog)
 logger.addHandler(filelog)
 
+# Regexs we will need
+LEADING_DIGIT_CLEANER = re.compile("^\(?\d+\.?\)?\s*")
+OTHER_LEADING_DIGIT_CLEANER = re.compile("^\d+\s*\-?\s*")
+BRACKET_CLEANER = re.compile("\s*[\[|(].*[\]|)]\s*")
+ISSUE_RANGE_CLEANER = re.compile("\s*\d+\s*-\s*\d+\s*")
+VOLUME_CLEANER = re.compile("\s*v\s{0,1}\d+\s*", re.IGNORECASE)
+LONELY_APOSTROPHE_CLEANER = re.compile("\s+'\W*\s*")
+HIGH_ASCII_CLEANER = re.compile("[^\\x00-\\x7f]")
+ANNUALS_CLEANER = re.compile("[\s|-]+annuals.*", re.IGNORECASE)
+
 class CBRResource(resource.Resource):
     isLeaf = True
     
@@ -46,6 +57,7 @@ class CBRResource(resource.Resource):
         # ASSUMPTION: Empty folders (parents that only contain other folders or
         # non-matching files) should never be used as a key in TITLES
         self.ignored_folder_names = []
+        total = 0
         
         # when you find a cbr or cbz, put folder name into titles
         # problem here: fnmatch is only case-insensitive on case-insensitive OSes - replace?
@@ -56,27 +68,82 @@ class CBRResource(resource.Resource):
                 self.ignored_folder_names.append(os.path.split(root)[-1])
             for f in matches:
                 self._add_match_to_collection(f, root)
-        logger.info("Found %d comics" % len(matches))
+                total = total + 1
+        logger.info("Found %d comics" % total)
 
     def render_GET(self, request):
         request.setHeader("content-type", "text/html")
         response = "Serving contents of %s" % self.directory
-        for d in self.files:
-            response += "<br />%s" % d
+        for key in sorted(self.titles.iterkeys()):
+            entry = self.titles[key]
+            #response += "<br />%s: %d issues" % (key, entry["count"])
+            response += "<br />%s" % (key)
         return response
     
     def _add_match_to_collection(self, filename, root):
+        """
+        For a matching file, look at its folder information. If any of the folders
+        in its parent path exist in self.titles already, use that. Otherwise,
+        create a new entry.
+        
+        Update book count and add this match to self.titles[key] files list
+        
+        TODO: The problem with this folder-as-title logic is that its dependent
+        on the order in which the folders are matched by fnmatch.filter
+        As an example, ideally all of the titles I have under E:\Comics\Indies\Nexus
+        would be inside 'Nexus', but because the sub-folders are fed in first, only
+        the one file in the root folder shows up there
+        """
         path_info = os.path.split(root.replace(self.directory, ""))
         exists = False
         for folder in path_info:
             if folder in self.ignored_folder_names:
                 continue
+            folder = self._prep_title(folder)
             if self.titles.has_key(folder):
                 exists = True
                 self.titles[folder]["count"] = self.titles[folder]["count"] + 1
         if not exists or not self.titles.has_key(folder):
             self.titles[folder] = {"count": 1, "files": []}
-        self.titles[folder]["files"].append(os.path.join(root, filename))
+        
+        # ignore duplicate files
+        file_list = self.titles[folder]["files"]
+        file_path = os.path.join(root, filename)
+        if file_path not in file_list:
+            file_list.append(file_path)
+    
+    def _prep_title(self, folder_name):
+        """
+        Do some basic cleanup on the nastiness P2P folks and anal-retentives
+        like to add to folders.
+        
+        * Get rid of underscores and # signs
+        * leading digits for sorted collections (e.g., '1. A New Hope')
+            how to differentiate this from a comic like 2000 A.D.?
+            especially when people put years in the titles
+        * Strip anything inside brackets or parens
+        * Lose any number ranges (e.g., 1 - 10), regardless of space inside
+        * Get rid of volume indicators for now (e.g., v2)
+            might need to provide that info later in titles
+        * Get rid of any apostrophes we've orphaned (e.g., '93-'96)
+        * Lose high ASCII, non-alphanumeric stuff (e.g., copyright symbol)
+        * Strip "Annuals" to prevent having a separate folder for those ???
+        * If we're left with nothing at the end, return the original folder name
+        """
+        title = folder_name.replace("_", " ")
+        title = folder_name.replace("#", "")
+        title = LEADING_DIGIT_CLEANER.sub("", title)
+        title = OTHER_LEADING_DIGIT_CLEANER.sub("", title)
+        title = BRACKET_CLEANER.sub("", title)
+        title = ISSUE_RANGE_CLEANER.sub("", title)
+        title = VOLUME_CLEANER.sub("", title)
+        title = LONELY_APOSTROPHE_CLEANER.sub("", title)
+        title = HIGH_ASCII_CLEANER.sub("", title)
+        title = ANNUALS_CLEANER.sub("", title)
+        
+        if not len(title):
+            return folder_name
+        return title
     
     def _normalize_directory_path(self, directory):
         """
